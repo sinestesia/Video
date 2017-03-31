639fb7edc2de31b09552044f4cad7fdecd77d04c
sinestesia <reyesdelsuenio@gmail.com>
2017-03-31 19:37:07 +0200

    Ejemplo reproduccion youtube

---
+++ app/src/main/java/es/pamp/video/MainActivity.java
@@ -0,0 +1,25 @@
+package es.pamp.video;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button videoBoton=(Button) findViewById(R.id.videoBoton);
+        videoBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getIntent().ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=cvyywDE67tY")));
+            }
+        });
+    }
+}
